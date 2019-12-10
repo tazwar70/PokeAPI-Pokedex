ac9393248a8a2462cbea71c659c0b072bb2b90ff
Md Shadman Tajwar Haque <36808747+tazwar70@users.noreply.github.com>
2019-12-11 00:06:59 +0600

    Add files via upload
    A Pokedex made using the Pokedex API.

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const pokedex = document.getElementById('pokedex');
 
+var firstPokemon = 1;
+var lastPokemon = 151;
+
+
+
 const fetchPokemon = () => {
     const promises = [];
-    for (let i = 1; i <= 807; i++) {
+    for (let i = firstPokemon; i <= lastPokemon; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         promises.push(fetch(url).then((res) => res.json()));
     }
@@ -23,8 +28,9 @@ const displayPokemon = (pokemon) => {
         .map(
             (pokeman) => `
         <li class="card">
+            <h2 class="card-title">#${pokeman.id}</h2>
+            <h1 class="pokemon-name">${pokeman.name}</h1>
             <img class="card-image" src="${pokeman.image}"/>
-            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
             <p class="card-subtitle">Type: ${pokeman.type}</p>
         </li>
     `
@@ -34,3 +40,39 @@ const displayPokemon = (pokemon) => {
 };
 
 fetchPokemon();
+
+function kantoDex(){
+    firstPokemon = 1;
+    lastPokemon = 151;
+    fetchPokemon();
+};
+function jhotoDex(){
+    firstPokemon = 152;
+    lastPokemon = 251;
+    fetchPokemon();
+};
+function hoennDex(){
+    firstPokemon = 252;
+    lastPokemon = 386;
+    fetchPokemon();
+};
+function sinnohDex(){
+    firstPokemon = 387;
+    lastPokemon = 493;
+    fetchPokemon();
+};
+function unovaDex(){
+    firstPokemon = 494;
+    lastPokemon = 649;
+    fetchPokemon();
+};
+function kalosDex(){
+    firstPokemon = 650;
+    lastPokemon = 721;
+    fetchPokemon();
+};
+function alolaDex(){
+    firstPokemon = 722;
+    lastPokemon = 809;
+    fetchPokemon();
+};
